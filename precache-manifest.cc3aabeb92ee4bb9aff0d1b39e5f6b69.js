bf39450b7fd25d56e3dc39888fa5359a4e6aaca2
David Llop <931684+sembrestels@users.noreply.github.com>
2019-05-30 14:13:35 +0200

    Updates

---
+++ precache-manifest.cc3aabeb92ee4bb9aff0d1b39e5f6b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6cea457b162958312f7c",
+    "url": "/smart-projects/static/js/runtime~main.3d5fea63.js"
+  },
+  {
+    "revision": "c4e5a663f7dc9fa34abc",
+    "url": "/smart-projects/static/js/main.c3ab895e.chunk.js"
+  },
+  {
+    "revision": "88a1e4ec42c56ca6fba9",
+    "url": "/smart-projects/static/js/2.991950d9.chunk.js"
+  },
+  {
+    "revision": "c4e5a663f7dc9fa34abc",
+    "url": "/smart-projects/static/css/main.774d932d.chunk.css"
+  },
+  {
+    "revision": "3077657f7221bf79f8c6dfd6af5c9650",
+    "url": "/smart-projects/index.html"
+  }
+];
